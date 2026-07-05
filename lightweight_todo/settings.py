0ef0531f7b483ebe42d0e07f0863bd6e3da9ddc2
Tharun Gowda <tharungowda0202@gmail.com>
2026-07-06 00:28:29 +0530

    Reset SQLite database
    # Conflicts:
    #	db.sqlite3

--- lightweight_todo/settings.py
+++ lightweight_todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7%xacc0zh6u=%k7=mk#a8(iq7p&=_wr=!a0=d=_&i%nn7oewz1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
